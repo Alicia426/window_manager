3edb5025281f5ec5437db31685c1aecf6d1f3083
Alicia426 <aliciakim1995@gmail.com>
2025-05-15 02:53:26 -0600

    Debian script

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,3 +1,5 @@
 wget https://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/amd64/albert_0.27.8-0+701.1_amd64.deb
-sudo apt install ./albert_0.27.8-0+701.1_amd64.deb
+sudo apt install xorg -y
+sudo apt install libx11-dev -y
+sudo apt install ./albert_0.27.8-0+701.1_amd64.deb -y
 echo "exec albert" >> ~/.xinitrc
