6a703748c67f19b851e2383dfa56099079e4bdae
Alicia426 <aliciakim1995@gmail.com>
2025-05-15 10:41:52 -0600

    Updated script

--- debian_setup_python.sh
+++ debian_setup_python.sh
@@ -1,8 +1,17 @@
-wget https://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/amd64/albert_0.27.8-0+701.1_amd64.deb
+# Debian and Ubuntu Setup
+
+FILE="albert_0.27.8-0+701.1_amd64.deb"     
+if [ -f $FILE ]; then
+   echo "File $FILE exists already, skipping download"
+else
+   echo "Downloading $FILE"
+   wget https://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/amd64/albert_0.27.8-0+701.1_amd64.deb
+fi
+
 sudo apt install xorg -y
 sudo apt install libx11-dev -y
 sudo apt install ./albert_0.27.8-0+701.1_amd64.deb -y
-sudo apt install pop-icon-theme -y
+# sudo apt install pop-icon-theme -y
 sudo apt install build-essential libssl-dev libffi-dev python3 python3-dev
 sudo apt install python3-xlib
 make
